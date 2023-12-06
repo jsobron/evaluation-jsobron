4cf610aabd54f1297ca434d617b574a013732bdf
JuanSobron01 <sobronjuancruz@gmail.com>
2023-12-06 17:50:07 -0300

    ADD test_Tecnico funcion nro3 refactorizada & agregada exceptions & justificacion

--- models/models.py
+++ models/models.py
@@ -24,95 +24,107 @@ def _get_packet_publishable_product_type(self):
 #  Refactorizar
 #  Agregar una condición para cualquier otro valor de 'publishable_product_type' lanzar una excepción de Odoo
 #  Sustentar el tipo de excepción que usó(¿Por que?)
+
+# 3. Use de excepction el UserError es mas amigable a la vista del usuario.
+from odoo.exceptions import UserError
 def packet_product_action_publish(self):
     for rec in self:
         is_lan = False
-        parameter_ids = self.env['guru.rule.country'].search([('code', '=', 'LAN')])
+        parameter_ids = self.env['guru.rule.country'].browse([('code', '=', 'LAN')])
         if self.env.company.country_id.id in parameter_ids.country_ids.ids:
             is_lan = True
-        data_product = self.env['product.product'].search([('id', '=', rec.guru_product_id.id)])
-        is_product_packet = True
-        for data in data_product:
-            if data.publishable_ok:
-                if data.product_tmpl_id.publishable_product_type == "pacom":
-                    wzd_id = self.env['guru.pacom.wzd'].create({'var_obj': 'Obj', 'is_lan': is_lan})
-                    return {
-                        'name': _('Pacom'),
-                        'view_mode': 'form',
-                        'view_type': 'form',
-                        'view_id': [self.env.ref('guru_pacom.view_guru_pacom_simple_wzd_form').id],
-                        'res_model': 'guru.pacom.wzd',
-                        'type': 'ir.actions.act_window',
-                        'target': 'new',
-                        'res_id': wzd_id.id,
-                        'context': {
-                            'packet_product_line': rec.id,
-                            'line_id': rec.order_line_id.id,
-                            'order_id': rec.order_id.id,
-                            'partner_id': rec.order_id.partner_id.id,
-                        },
-                    }
-                if data.product_tmpl_id.publishable_product_type == "landing_pacom":
-                    wzd_landing_id = self.env['guru.landing.pacom.wzd'].create({'var_obj': 'Obj'})
-                    return {
-                        'name': _('Landing Pacom'),
-                        'view_mode': 'form',
-                        'view_type': 'form',
-                        'view_id': [self.env.ref('guru_pacom.guru_landing_pacom_wzd_form_view').id],
-                        'res_model': 'guru.landing.pacom.wzd',
-                        'type': 'ir.actions.act_window',
-                        'target': 'new',
-                        'res_id': wzd_landing_id.id,
-                        'context': {
-                            'packet_product_line': rec.id,
-                            'line_id': rec.order_line_id.id,
-                            'order_id': rec.order_id.id,
-                            'partner_id': rec.order_id.partner_id.id,
-                        },
-                    }
-                if data.product_tmpl_id.publishable_product_type == "web_shop":
-                    wzd_web_id = self.env['guru.web.product.wzd'].create({'var_obj': 'Obj', 'is_lan': is_lan})
-                    line_id = rec.order_line_id.product_id.guru_packet_ids.filtered(
-                        lambda e: e.publishable_product_type == 'web_shop')
-                    wzd_web_id.write({'plan_id': line_id.guru_product_id.plan_duda_id.id})
-                    return {
-                        'name': _('Webs Shop'),
-                        'view_mode': 'form',
-                        'view_type': 'form',
-                        'view_id': [self.env.ref('guru_pacom.guru_web_wzd_form_view').id],
-                        'res_model': 'guru.web.product.wzd',
-                        'type': 'ir.actions.act_window',
-                        'target': 'new',
-                        'res_id': wzd_web_id.id,
-                        'context': {
-                            'packet_product_line': rec.id,
-                            'line_id': rec.order_line_id.id,
-                            'order_id': rec.order_id.id,
-                            'partner_id': rec.order_id.partner_id.id,
-                        },
-                    }
-                if data.product_tmpl_id.publishable_product_type == "web_vertical":
-                    wzd_web_id = self.env['guru.web.vertical.wzd'].create({'var_obj': 'Obj', 'is_lan': is_lan})
-                    line_id = rec.order_line_id.product_id.guru_packet_ids.filtered(
-                        lambda e: e.publishable_product_type == 'web_vertical')
-                    return {
-                        'name': _('Tiendas WIX'),
-                        'view_mode': 'form',
-                        'view_type': 'form',
-                        'view_id': [self.env.ref('guru_pacom.guru_web_vertical_wzd_form_view').id],
-                        'res_model': 'guru.web.vertical.wzd',
-                        'type': 'ir.actions.act_window',
-                        'target': 'new',
-                        'res_id': wzd_web_id.id,
-                        'context': {
-                            'packet_product_line': rec.id,
-                            'line_id': rec.order_line_id.id,
-                            'order_id': rec.order_id.id,
-                            'partner_id': rec.order_id.partner_id.id,
-                        },
-                    }
+        data_product = self.env['product.product'].browse([('id', '=', rec.guru_product_id.id)])
+        #Verifico si hay data_producto o si es el publicable o no
+        if not data_product or not data_product.publishable_ok:
+            raise UserError("El producto no es publicable")
+        
+
+        product_type = data_product.product_tmpl_id.publishable_product_type
+        if product_type == 'pacom': 
+            wzd_id = self.env['guru.pacom.wzd'].create({'var_obj': 'Obj', 'is_lan': is_lan})
+            return {
+                'name': _('Pacom'),
+                'view_mode': 'form',
+                'view_type': 'form',
+                'view_id': [self.env.ref('guru_pacom.view_guru_pacom_simple_wzd_form').id],
+                'res_model': 'guru.pacom.wzd',
+                'type': 'ir.actions.act_window',
+                'target': 'new',
+                'res_id': wzd_id.id,
+                'context': {
+                    'packet_product_line': rec.id,
+                    'line_id': rec.order_line_id.id,
+                    'order_id': rec.order_id.id,
+                    'partner_id': rec.order_id.partner_id.id,
+                },
+            }
+        elif product_type == 'landing_pacom':
+            wzd_landing_id = self.env['guru.landing.pacom.wzd'].create({'var_obj': 'Obj'})
+            return {
+                'name': _('Landing Pacom'),
+                'view_mode': 'form',
+                'view_type': 'form',
+                'view_id': [self.env.ref('guru_pacom.guru_landing_pacom_wzd_form_view').id],
+                'res_model': 'guru.landing.pacom.wzd',
+                'type': 'ir.actions.act_window',
+                'target': 'new',
+                'res_id': wzd_landing_id.id,
+                'context': {
+                    'packet_product_line': rec.id,
+                    'line_id': rec.order_line_id.id,
+                    'order_id': rec.order_id.id,
+                    'partner_id': rec.order_id.partner_id.id,
+                },
+            }
+        
+        elif product_type =='web_shop':
+            wzd_web_id = self.env['guru.web.product.wzd'].create({'var_obj': 'Obj', 'is_lan': is_lan})
+            line_id = rec.order_line_id.product_id.guru_packet_ids.filtered(
+                lambda e: e.publishable_product_type == 'web_shop')
+            wzd_web_id.write({'plan_id': line_id.guru_product_id.plan_duda_id.id})
+            return {
+                'name': _('Webs Shop'),
+                'view_mode': 'form',
+                'view_type': 'form',
+                'view_id': [self.env.ref('guru_pacom.guru_web_wzd_form_view').id],
+                'res_model': 'guru.web.product.wzd',
+                'type': 'ir.actions.act_window',
+                'target': 'new',
+                'res_id': wzd_web_id.id,
+                'context': {
+                    'packet_product_line': rec.id,
+                    'line_id': rec.order_line_id.id,
+                    'order_id': rec.order_id.id,
+                    'partner_id': rec.order_id.partner_id.id,
+                },
+            }
+        
+        elif product_type == 'web_vertical':
+            wzd_web_id = self.env['guru.web.vertical.wzd'].create({'var_obj': 'Obj', 'is_lan': is_lan})
+            line_id = rec.order_line_id.product_id.guru_packet_ids.filtered(
+                lambda e: e.publishable_product_type == 'web_vertical')
+            return {
+                'name': _('Tiendas WIX'),
+                'view_mode': 'form',
+                'view_type': 'form',
+                'view_id': [self.env.ref('guru_pacom.guru_web_vertical_wzd_form_view').id],
+                'res_model': 'guru.web.vertical.wzd',
+                'type': 'ir.actions.act_window',
+                'target': 'new',
+                'res_id': wzd_web_id.id,
+                'context': {
+                    'packet_product_line': rec.id,
+                    'line_id': rec.order_line_id.id,
+                    'order_id': rec.order_id.id,
+                    'partner_id': rec.order_id.partner_id.id,
+                },
+            }
+        
+        else:
+            raise UserError(f"El tipo de producto '{product_type}' no se reconoce.")
 
 # TODO 04:
 #  Refactorizar
 def get_select_sequence(self):
+
     return [(str(i), str(i)) for i in range(1, 11)]
